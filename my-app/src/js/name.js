9593a1be9c515a89fdfe4e343e3d2730887e8e2d
whitelotus19 <159101935+centmarde@users.noreply.github.com>
2024-04-26 01:14:11 +0800

    added Router,Login,Fixed Register (#1)

--- my-app/src/js/name.js
+++ my-app/src/js/name.js
@@ -6,6 +6,10 @@ import * as bootstrap from 'bootstrap'
 
 import { createClient } from '@supabase/supabase-js'
 
+import { setRouter } from './router/router'; //router para dile mo gawas if naka login na
+
+setRouter(); //uncomment for disable router
+
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://vlzwiqqexbsievtuzfgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsendpcXFleGJzaWV2dHV6ZmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4ODUzMDYsImV4cCI6MjAyODQ2MTMwNn0.kxsiLG__FkxF2DGhuUk1Ritc6tnt-W-S0qG4kizkDfI')
 
