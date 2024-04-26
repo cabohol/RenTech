5540faf437ba1455f825bb1b35c9e08ce5da7466
whitelotus19 <centmarde.campado@gmail.com>
2024-04-27 00:40:12 +0800

    fixed SignUP Fetch

--- my-app/src/js/auth/login.js
+++ my-app/src/js/auth/login.js
@@ -3,55 +3,48 @@ import { supabase } from "../name";
 const form_login = document.getElementById("emailform");
 
 form_login.onsubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
+  const formData = new FormData(form_login);
 
-    const formData = new FormData(form_login);
+  //supabase sign-in
 
-    //supabase sign-in
-    
-    let { data, error } = await supabase.auth.signInWithPassword({
-        email: formData.get("email"),
-        password: formData.get("password"),
-    });
+  let { data, error } = await supabase.auth.signInWithPassword({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 
-    let session = data.session;
-    let user = data.user;
-    
-    console.log(user);
+  let session = data.session;
+  let user = data.user;
 
-    if (session != null) {
-        // Store tokens for API
-        localStorage.setItem("access_token", session.access_token);
-        localStorage.setItem("refresh_token", session.refresh_token);
+  console.log(user);
 
-       
-      
+  if (session != null) {
+    // Store tokens for API
+    localStorage.setItem("access_token", session.access_token);
+    localStorage.setItem("refresh_token", session.refresh_token);
 
-        // For role-based authentication; uncomment if you want to implement example: admin log-in
-        let { data: userinformation, error } = await supabase
-            .from("userinformation")
-            .select("*")
-    
-         // Save user id in local storage
-        localStorage.setItem("user_id", userinformation[0].id);
-        console.log(userinformation[0].id);
+    // For role-based authentication; uncomment if you want to implement example: admin log-in
+    localStorage.setItem("auth_id", user?.id);
 
+    // Fetch user profiles
+    let { data: userinformation, error } = await supabase
+      .from("userinformation")
+      .select("*")
+      .eq("user_id", localStorage.getItem("auth_id"));
 
-        if (session != null) {
-            const userRole = userinformation[0].Role;
-            const userId = user.id;
+    localStorage.setItem("user_id", userinformation[0].id);
+    console.log(userinformation[0].id);
 
-            alert("Login Successfully");
-            window.location.pathname = '/home.html';
-        }
-    } else {
-        alert(`Error: ${error.message}`);
-        console.log(error);
-    }
+    // Redirect to home page after successful login
+    window.location.pathname = "/home.html";
 
-    // Resetting form
-    form_login.reset();
+    alert("Login Successfully");
+  } else {
+    alert(`Error: ${error.message}`);
+    console.log(error);
+  }
 
-    
+  // Resetting form
+  form_login.reset();
 };
