5e94b16bdeeef48d98591c1d152eaa17d54d9a96
whitelotus19 <159101935+centmarde@users.noreply.github.com>
2024-04-27 00:46:24 +0800

    Master (#11)
    * added Logout and Fixed some issues
    
    * fixed SignUP Fetch

--- my-app/src/js/auth/signup.js
+++ my-app/src/js/auth/signup.js
@@ -3,71 +3,80 @@ import { supabase } from "../name";
 const form_signup = document.getElementById("form_signup");
 
 form_signup.onsubmit = async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const formData = new FormData(form_signup);
-    const password = formData.get("password");
-    const email = formData.get("email");
-    const firstName = formData.get("first_name");
-    const lastName = formData.get("last_name");
-    const contactNumber = formData.get("contact_number");
-    const fbLink = formData.get("fb_link");
-    const collegeName = formData.get("college_name");
-    const programName = formData.get("program_name");
-    
-    if (password == formData.get("password_confirmation")) {
-        const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
-            email: email,
-            password: password,
-        });
+  const formData = new FormData(form_signup);
+  const password = formData.get("password");
+  const email = formData.get("email");
+  const firstName = formData.get("first_name");
+  const lastName = formData.get("last_name");
+  const contactNumber = formData.get("contact_number");
+  const fbLink = formData.get("fb_link");
+  const collegeName = formData.get("college_name");
+  const programName = formData.get("program_name");
 
-        let userId = signUpData?.user?.id;
+  if (password == formData.get("password_confirmation")) {
+    const { data: signUpData, error: signUpError } = await supabase.auth.signUp(
+      {
+        email: email,
+        password: password,
+      }
+    );
 
-        if (userId != null) {
-            const { data: userInfoData, error: userInfoError } = await supabase
-                .from('userinformation')
-                .insert([
-                    {
-                        first_name: firstName,
-                        last_name: lastName,
-                        contact_number: contactNumber,
-                        fb_link: fbLink,
-                        user_id: userId,
-                    },
-                ]);
-                /* const Id = userInfoData[0].id; */
-               /*  console.log(Id);
- */
-            const { data: collegeData, error: collegeError } = await supabase
-                .from('college')
-                .insert([
-                    {
-                        college_name: collegeName,
-                      /*   user_id: Id, */
-                    },
-                ]);
+    let userId = signUpData?.user?.id;
 
-               /*  const college_id = collegeData[0].id; */
-                /* console.log(college_id); */
+    if (userId != null) {
+      const { data: userInfoData, error: userInfoError } = await supabase
+        .from("userinformation")
+        .insert([
+          {
+            first_name: firstName,
+            last_name: lastName,
+            contact_number: contactNumber,
+            fb_link: fbLink,
+            user_id: userId,
+          },
+        ]);
 
-            const { data: programData, error: programError } = await supabase
-                .from('program')
-                .insert([
-                    {
-                        program_name: programName,
-                       /*  college_id: college_id, */
-                    },
-                ]);
+      let { data: profiles, error } = await supabase
+        .from("userinformation")
+        .select("*")
+        .eq("user_id", userId);
+      const Id = profiles[0].id;
+      console.log(Id);
 
-           
-            alert("Sign Up Successful");
-            window.location.href="login.html";
-            
-        } else {
-            alert(`Error: ${signUpError.message}`);
-            console.log(signUpError);
-        }
+      const { data: collegeData, error: collegeError } = await supabase
+        .from("college")
+        .insert([
+          {
+            college_name: collegeName,
+            user_id: Id,
+          },
+        ]);
+
+      let { data: college, error: FetchcollegeError } = await supabase
+        .from("college")
+        .select("*")
+        .eq("user_id", Id);
+      const college_id = college[0].id;
+      console.log(college_id);
+
+      const { data: programData, error: programError } = await supabase
+        .from("program")
+        .insert([
+          {
+            program_name: programName,
+            college_id: college_id,
+          },
+        ]);
+
+      alert("Sign Up Successful");
+      window.location.href = "login.html";
     } else {
-        alert("Password doesn't match");
+      alert(`Error: ${signUpError.message}`);
+      console.log(signUpError);
     }
+  } else {
+    alert("Password doesn't match");
+  }
 };
