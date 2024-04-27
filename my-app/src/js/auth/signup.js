6f85097c41cacdc31a83da82d17c708952258a7b
kleyr567 <claireannebohol567@gmail.com>
2024-04-27 09:00:33 +0800

    notification

--- my-app/src/js/auth/signup.js
+++ my-app/src/js/auth/signup.js
@@ -1,58 +1,85 @@
-import { supabase } from "../name";
+import { supabase, successNotification, errorNotification } from "../name";
 
 const form_signup = document.getElementById("form_signup");
 
 form_signup.onsubmit = async (e) => {
     e.preventDefault();
 
+    //Disable button
+    document.querySelector("#form_signup button").disabled = true;
+    document.querySelector("#form_signup button").innerHTML = '<div class="spinner-border me-2" role="status"></div> <span>Loading..</span>';
+
     const formData = new FormData(form_signup);
+    const password = formData.get("password");
+    const email = formData.get("email");
+    const firstName = formData.get("first_name");
+    const lastName = formData.get("last_name");
+    const contactNumber = formData.get("contact_number");
+    const fbLink = formData.get("fb_link");
+    const collegeName = formData.get("college_name");
+    const programName = formData.get("program_name");
+    
+    if (password == formData.get("password_confirmation")) {
+        const { data: signUpData, error: signUpError } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
 
-    if(formData.get("password") == formData.get("password_confirmation")){
-        alert("Password match");
+        let userId = signUpData?.user?.id;
 
-     const {data, error} = await supabase.auth.signUp({
-        email: formData.get("email"),
-        password: formData.get("password"),
-     });
-     
+        if (userId != null) {
+            const { data: userInfoData, error: userInfoError } = await supabase
+                .from('userinformation')
+                .insert([
+                    {
+                        first_name: firstName,
+                        last_name: lastName,
+                        contact_number: contactNumber,
+                        fb_link: fbLink,
+                        user_id: userId,
+                    },
+                ]);
+                /* const Id = userInfoData[0].id; */
+               /*  console.log(Id);
+ */
+            const { data: collegeData, error: collegeError } = await supabase
+                .from('college')
+                .insert([
+                    {
+                        college_name: collegeName,
+                      /*   user_id: Id, */
+                    },
+                ]);
 
-     let user_id = data?.user?.id;
+               /*  const college_id = collegeData[0].id; */
+                /* console.log(college_id); */
 
+            const { data: programData, error: programError } = await supabase
+                .from('program')
+                .insert([
+                    {
+                        program_name: programName,
+                       /*  college_id: college_id, */
+                    },
+                ]);
 
-        if(user_id != null){
-            const { data, error } = await supabase
-           .from('userinformation')
-           .insert([
-          { first_name: formData.get("first_name") ,
-           last_name: formData.get("last_name") ,
-           contact_number: formData.get("contact_number"),
-           fb_link: formData.get("fb_link") ,
-           user_id: user_id,
-        },
-        ])
-        .select()
-        }else{
-            alert("error fetching");
-            console.log(error);
+            successNotification("Sign up Successfully!", 10);
+            //alert("Sign Up Successful");
+           // window.location.href="login.html";
+           
+        } else {
+            errorNotification("Cannot register account.", 10);
+            //alert(`Error: ${signUpError.message}`);
+            console.log(signUpError);
         }
+       //Reset Form
+         form_signup.reset();
 
-
-        
-     
-
-
-
-
-
-
-
-     console.log(data);
-     console.log(error);
-
-
-    }else{
-        alert("password doesnt match");
-        console.log(error);
+         //Enable submit button
+         document.querySelector("#form_signup button").disabled = false;
+         document.querySelector("#form_signup button").innerHTML = 'Sign up';
+   
+        } else {
+        alert("Password doesn't match");
     }
-
 };
--- my-app/src/js/name.js
+++ my-app/src/js/name.js
@@ -6,7 +6,61 @@ import * as bootstrap from 'bootstrap'
 
 import { createClient } from '@supabase/supabase-js'
 
+import { setRouter } from './router/router'; //router para dile mo gawas if naka login na
+
+setRouter(); //uncomment for disable router
+
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://vlzwiqqexbsievtuzfgm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsendpcXFleGJzaWV2dHV6ZmdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4ODUzMDYsImV4cCI6MjAyODQ2MTMwNn0.kxsiLG__FkxF2DGhuUk1Ritc6tnt-W-S0qG4kizkDfI')
 
-export {supabase};
+
+function successNotification(message, seconds = 0){
+  document.querySelector(".alert-success").classList.remove("d-none");
+  document.querySelector(".alert-success").classList.add("d-block");
+  document.querySelector(".alert-success").innerHTML = message;
+
+  if(seconds != 0){
+    setTimeout(function () {
+      document.querySelector(".alert-success").classList.remove("d-block");
+      document.querySelector(".alert-success").classList.add("d-none");
+
+    }, seconds * 1000);
+  }
+
+}
+
+function errorNotification(message, seconds = 0){
+  document.querySelector(".alert-danger").classList.remove("d-none");
+  document.querySelector(".alert-danger").classList.add("d-block");
+  document.querySelector(".alert-danger").innerHTML = message;
+
+  if(seconds != 0){
+    setTimeout(function () {
+      document.querySelector(".alert-danger").classList.remove("d-block");
+      document.querySelector(".alert-danger").classList.add("d-none");
+
+    }, seconds * 1000);
+  }
+
+}
+
+
+
+
+
+
+async function doLogout() {
+    let { error } = await supabase.auth.signOut();
+  
+    if (error == null) {
+      alert("Logout Successfully!");
+  
+      localStorage.clear();
+  
+      window.location.pathname = "/login.html";
+    } else {
+      alert("Logout Failed!");
+    }
+  }
+  
+  export { supabase, doLogout, successNotification, errorNotification };
